8e8ee4dca255785e6d6489529f3e3964e21545c1
sandhybotla <botla.sandhyarani46@gmail.com>
2026-05-21 07:55:53 +0530

    Add files via upload

---
+++ if condition practies.py
@@ -0,0 +1,26 @@
+#password=9848
+#key=input(input("enter 4 digit password"))
+#if (key == password):
+ # pint("loging successfull!!!")
+ #
+#num= int(input("enter the number:"))
+#if num %2 ==0:
+ #    print(num," is even ❤️")
+#else:
+  #  print(num," is odd")
+#   
+# num = int(input("enter anumber: "))
+# if num < 10 :
+#     print(num)
+# else:
+#     last = num % 10
+#     print(last)
+
+#
+#ch=input("enter a single character: ")
+#if ch.lower()in "aeiou":
+#    print("it is vowel")
+#else:
+ #   print("it is not a vowel")
+ 
+    
