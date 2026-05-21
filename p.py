4ee9b1a2d76f2b8eb0c382dc5fd36fc5384e0bda
sandhybotla <botla.sandhyarani46@gmail.com>
2026-05-21 07:56:48 +0530

    Add files via upload

---
+++ p.py
@@ -0,0 +1,7 @@
+n=int(input("enter n value:"))
+for i in range (n):
+    for j in range(n):
+        if i==0 or j==0 or(i==n//2 and j<4) or (j==4 and i<n//2 and i!=0):
+             print("*",end=" ")
+        else:
+            print(" ",end=" ")
